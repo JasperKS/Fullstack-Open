dfaeba150fc0798518dcf2e1fac90bfaebb93367
jasperks <98431887+JasperKS@users.noreply.github.com>
2024-11-07 18:32:28 +0000

    "filtering"

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,55 +1,55 @@
-import { useState } from 'react'
-import Person from './components/Person'
+import { useState } from "react";
+import Person from "./components/Person";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [searchInfo, setSearchInfo] = useState('')
-  const [filteredPeople, setFilteredPeople] = useState(persons)
-  
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Lightly", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchInfo, setSearchInfo] = useState("");
+  const [filteredPeople, setFilteredPeople] = useState(persons);
 
   const addName = (event) => {
     event.preventDefault();
     const newPerson = {
       name: newName,
       number: newNumber,
-      id: persons.length + 1
-    }
+      id: persons.length + 1,
+    };
 
-    if (persons.some(person => person.name == newName)) {
-      alert(`${newName} is already added to phonebook`)
+    if (persons.some((person) => person.name == newName)) {
+      alert(`${newName} is already added to phonebook`);
     } else {
       setPersons(persons.concat(newPerson));
-      setNewName('');
-      setNewNumber('');
+      setNewName("");
+      setNewNumber("");
     }
-  }
+  };
 
   const filterPeople = (event) => {
     event.preventDefault();
-    const filtered = persons.filter(person => person.name.toLowerCase().includes(searchInfo.toLowerCase()));
+    const filtered = persons.filter((person) =>
+      person.name.toLowerCase().includes(searchInfo.toLowerCase())
+    );
     setFilteredPeople(filtered);
-    setSearchInfo('');
-  }
+    setSearchInfo("");
+  };
 
   const handleNewName = (event) => {
     setNewName(event.target.value);
-  }
+  };
 
   const handleNewNumber = (event) => {
     setNewNumber(event.target.value);
-  }
+  };
 
   const handleFilter = (event) => {
     setSearchInfo(event.target.value);
-  }
-  
+  };
 
   return (
     <div>
@@ -57,24 +57,18 @@ const App = () => {
       <form onSubmit={filterPeople}>
         <div>
           filter shown with
-            <input
-              value={searchInfo}
-              onChange={handleFilter}/>
+          <input value={searchInfo} onChange={handleFilter} />
         </div>
       </form>
       <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
-          name: 
-            <input
-             value={newName}
-             onChange={handleNewName}/>
+          name:
+          <input value={newName} onChange={handleNewName} />
         </div>
         <div>
           number:
-            <input 
-              value={newNumber}
-              onChange={handleNewNumber}/>
+          <input value={newNumber} onChange={handleNewNumber} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -82,13 +76,13 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {filteredPeople.map(person =>
-          <Person key={person.id} person={person}/>
-        )}
+        {filteredPeople.map((person) => (
+          <Person key={person.id} person={person} />
+        ))}
       </ul>
       <div>debug: {newName}</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
