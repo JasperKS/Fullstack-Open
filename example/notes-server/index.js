434d824eb80dbb5fa75b266167b211f3d5eb0d71
JasperKS <kjdlsarmiento@gmail.com>
2024-12-09 05:42:45 -0500

    webserver example done

--- example/notes-server/index.js
+++ example/notes-server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
     {
       id: "1",
@@ -36,6 +38,35 @@ app.get('/api/notes/:id', (request, response) => {
     } else {
         response.status(404).end();
     }
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => Number(n.id)))
+        : 0
+
+    return String(maxId + 1);
+}
+
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: Boolean(body.important) || false,
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+
+    response.json(note)
 })
 
 app.delete('/api/notes/:id', (request, response) => {
